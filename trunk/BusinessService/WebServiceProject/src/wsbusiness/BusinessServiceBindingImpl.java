a563da8277bf637e266723d0d15ca121c50e1bda
joaquinpf <joaquinpf@be2d005e-d45c-11dd-a3c7-fda619b15ae3>
2009-02-15 14:12:24 +0000

    Parte 3: Business service

---
+++ trunk/BusinessService/WebServiceProject/src/wsbusiness/BusinessServiceBindingImpl.java
@@ -0,0 +1,19 @@
+/**
+ * BusinessServiceBindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package wsbusiness;
+
+public class BusinessServiceBindingImpl implements wsbusiness.BusinessService{
+    public double getDolarCotization(java.lang.String fromCurrency) throws java.rmi.RemoteException {
+        return -3;
+    }
+
+    public double[] convertCurrency(double amount, java.lang.String[] currencies) throws java.rmi.RemoteException {
+        return null;
+    }
+
+}
---
+++ trunk/BusinessService/WebServiceProject/src/wsbusiness/springclient/BusinessServiceIF.java
@@ -0,0 +1,5 @@
+package wsbusiness.springclient;
+
+public interface BusinessServiceIF {
+	
+}
