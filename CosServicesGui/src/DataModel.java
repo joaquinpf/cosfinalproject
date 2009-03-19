1d92f6544fdae76dc4ee94a9ab90522f9a5f2675
joaquinpf <joaquinpf@be2d005e-d45c-11dd-a3c7-fda619b15ae3>
2009-03-19 22:14:24 +0000

    Agregado el WSDL del ProjectTeamService. Agregado el proyecto netbeans de la interfaz grafica

---
+++ CosServicesGui/src/DataModel.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Hashtable;
+import wox.serial.Easy;
+
+/**
+ *
+ * @author Kireta
+ */
+public class DataModel {
+
+    public static DataModel INSTANCE = new DataModel();
+
+    private HashMap<String,String> users = new HashMap<String, String>();
+
+    protected DataModel(){
+        loadUsers();
+    }
+
+	private void saveUsers() {
+		Easy.save(users, "./config/users.xml");
+	}
+
+	private void loadUsers() {
+		File f = new File("./config/users.xml");
+		if (f.exists()) {
+			users = (HashMap<String,String>)Easy.load("./config/users.xml");
+		}
+	}
+
+    public boolean existUser(String key) {
+        return users.containsKey(key);
+    }
+
+    public String addUser(String key, String value) {
+        if(existUser(key)){
+        	return "Username already exists";
+        }
+    	users.put(key, value);
+        saveUsers();
+        return "User created succesfully";
+    }
+    
+    public boolean validateUser(String key, String value){
+    	if (existUser(key)){
+    		if(users.get(key).equals(value)){
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+}
