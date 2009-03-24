ff56193b48b4a2081a2e8a8afddf21411c63e07a
msteimbach <msteimbach@alumnos.exa.unicen.edu.ar>
2009-03-24 05:31:32 +0000

    Me cambiaste de lugar las cosas justo que hice un update... voy a hace el commit y no me encuentra la mitad de las cosas :S jaja Bue, esto es lo que hay y fala... ChatServiceLocalImp.java y MailServiceLocalImp.java estan implementados (falta la implementacion del metodo connect). MainChat.java, NewMail.java, ViewMessage.java, MailPanel.java y AddContact.java estan implementadas con la interfaz pero falta terminar de adaptarlas por medio de ChatServiceLocalImp.java y MailServiceLocalImp.java. Y por ultimo falta integrar los formularios principales de mail (MailPanel.java) y chat (MainChat.form) al formulario principal de la aplicacion. Parece mucho pero no es tanto... :p Espero no romper nada...

--- CosServicesGuiProxy/src/ar/com/cosgui/services/imp/ChatServiceLocalImp.java
+++ CosServicesGuiProxy/src/ar/com/cosgui/services/imp/ChatServiceLocalImp.java
@@ -1,8 +1,11 @@
 package ar.com.cosgui.services.imp;
 
 import ar.com.cosgui.services.IServiceLocalImp;
+import wschat.ChatServiceProxy;
+
 
 public class ChatServiceLocalImp implements IServiceLocalImp {
+	private ChatServiceProxy service = new ChatServiceProxy();
 
 	@Override
 	public void connect() {
@@ -11,7 +14,79 @@ public class ChatServiceLocalImp implements IServiceLocalImp {
 
 	@Override
 	public int addUser(String user, String pass) {
-		// TODO Auto-generated method stub
-		return 0;
+    	try {
+			if (this.validUserName(user) == 0)
+				return service.addUserMail(new Auth(user, pass));
+			else 
+				return 0;
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
 	}
+
+	public int login(String user, String pass) throws java.rmi.RemoteException{
+    	try {
+			return service.login (new Auth(user, pass));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	  
+	public int logout(String user, String pass) throws java.rmi.RemoteException{
+    	try {
+			return service.logout (new Auth(user, pass));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	  
+	public int sendMessage(String message, String usernameSrc, String usernameDst) throws java.rmi.RemoteException{
+    	try {
+			return service.sendMessage (new TextMessage(message, usernameSrc, usernameDst));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	  
+	public java.lang.String[] getContacts(String username) throws java.rmi.RemoteException{
+    	try {
+			return service.getContacts (username);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	  
+	public int addContact(String username, String contact) throws java.rmi.RemoteException{
+    	try {
+			if (!username.equals(contact))
+				return service.addContact (username, contact);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	  
+	public java.lang.String[] receiveMessage(String username) throws java.rmi.RemoteException{
+    	try {
+			return service.receiveMessage (username);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	  
+	public int getStatus(String username) throws java.rmi.RemoteException{
+    	try {
+			return service.getStatus (username);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	  
+	public int existUserName(String username) throws java.rmi.RemoteException{
+    	try {
+			return service.existUserName (username);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	  
 }
--- CosServicesGuiProxy/src/ar/com/cosgui/services/imp/MailServiceLocalImp.java
+++ CosServicesGuiProxy/src/ar/com/cosgui/services/imp/MailServiceLocalImp.java
@@ -1,8 +1,12 @@
 package ar.com.cosgui.services.imp;
 
 import ar.com.cosgui.services.IServiceLocalImp;
+import wsmail.MailServiceProxy;
+import wsmail.Auth;
+import wsmail.Mail;
 
 public class MailServiceLocalImp implements IServiceLocalImp {
+	private MailServiceProxy service = new MailServiceProxy();
 
 	@Override
 	public void connect() {
@@ -11,9 +15,75 @@ public class MailServiceLocalImp implements IServiceLocalImp {
 
 	@Override
 	public int addUser(String user, String pass) {
-		// TODO Auto-generated method stub
-		return 0;
+    	try {
+			if (this.validUserName(user) == 0)
+				return service.addUserMail(new Auth(user, pass));
+			else 
+				return 0;
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	
+	public int sendMail(String username, String password, String to, String subject, String text) throws java.rmi.RemoteException{
+        Mail mail = new Mail(username, to, subject, text, "new");
+        try {
+			if (this.validUser(username, password) == 1)
+				return service.sendMail(this.user, mail);
+			else
+				return 0;
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	 
+	public int validUser(String username, String password) throws java.rmi.RemoteException{
+        try {
+			return service.validUser(new Auth (username, password));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	  
+	public int validUserName(String user) throws java.rmi.RemoteException{
+		try {
+			return service.validUserName (user);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	  
+	public java.lang.String[] getMails(String username, String password) throws java.rmi.RemoteException{
+		try {
+			if (this.validUser(username, password) == 1)
+				return service.getMails (user);
+			else
+				return null;
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	  
+	public int saveMail(String username, String password, String from, String subject, String text, String status) throws java.rmi.RemoteException{
+		try {
+			if (this.validUser (username, password) == 1)
+				return service.saveMail (user, new Mail (from, username, subject, text, status));
+			else
+				return 0;
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	  
+	public int deleteMail(String username, String password, String from, String subject, String text) throws java.rmi.RemoteException{
+		try {
+			if (this.validUser (username, password) == 1)
+				return service.deleteMail (user, new Mail (from, username, subject, text, status));
+			else
+				return 0;
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+		
 }
